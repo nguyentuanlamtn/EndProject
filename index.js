38b3f87a0d881907e905b2806c697f009ded2590
nguyentuanlamtn <nguyentuanlamtn@gmail.com>
2021-12-29 19:43:56 +0700

    hi

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
 const result = document.getElementById("result");
-// const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
 btn.addEventListener("click", () => {
     let inpWord = document.getElementById("inp-word").value;
@@ -20,7 +19,6 @@ btn.addEventListener("click", () => {
                 <p class="word-example">
                     ${data[0].meanings[0].definitions[0].example || ""}
                 </p>`;
-            // sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
         })
         .catch(() => {
             result.innerHTML = `<h3 class="error">Không tìm thấy</h3>`;
